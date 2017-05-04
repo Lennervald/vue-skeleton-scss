98382f363f86d030f00bb2fd6c264de8fb09c5a9
Martin Lennervald <martin.lennervald@gmail.com>
2017-05-04 15:17:58 +0200

    added support for sass in vue-project

---
+++ src/main.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+// styles
+import './scss/skeleton/skeleton.scss';
+import './scss/main.scss'
+
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
